b6b3d8d52b771e9bd366ff56478cfd43d9832bce
Lucas123-len <lucasperangel@gmail.com>
2021-07-20 15:31:01 -0300

    exercicio javascript - par ou impar

---
+++ exercicio/parImpar.js
@@ -0,0 +1,9 @@
+function verificaParImpar() {
+    let numero = window.prompt("Insira um número")
+    if(numero%2 == 0){
+      alert("Numero digitado é par")
+    }else{
+      alert("Numero digitado é ímpar")
+    }
+  }
+  verificaParImpar()
