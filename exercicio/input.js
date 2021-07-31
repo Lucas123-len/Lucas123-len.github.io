9f841c926d5088cfb1f3791979b20fdca7d5eab1
Lucas123-len <lucasperangel@gmail.com>
2021-07-31 15:23:06 -0300

    exercicio javascript - DOM básico

---
+++ exercicio/input.js
@@ -0,0 +1,5 @@
+function carregamento() {
+    var input = document.querySelector('#textinput')
+    var label = document.querySelector('#label')
+    label.innerHTML = "Você está digitando "+input.value
+}
